e7e12db3b9e685ffd558b0a372504c95221fc6d5
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2026-03-07 19:12:35 +0000

    refactor: add missing coverage

--- crates/tribal-test-utils/src/mock/async_dispatch/repositories/retrieval_feedback.rs
+++ crates/tribal-test-utils/src/mock/async_dispatch/repositories/retrieval_feedback.rs
@@ -13,3 +13,23 @@ mock_repository! {
             (id: RetrievalFeedbackId) { id }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use tribal_db::RetrievalFeedbackRepository;
+
+    use super::*;
+
+    // -- Tests --------------------------------------------------------------
+
+    #[test]
+    fn test_send_sync_behind_arc() {
+        fn assert_send_sync<T: Send + Sync>() {}
+        assert_send_sync::<MockRetrievalFeedbackRepository>();
+
+        let mock = MockRetrievalFeedbackRepository::builder().build();
+        let _arc: Arc<dyn RetrievalFeedbackRepository + Send + Sync> = Arc::new(mock);
+    }
+}
